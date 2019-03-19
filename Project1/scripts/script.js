69adbda69e112606a8e5a241e890df7106721d86
DimasCoder <37441137+DimasCoder@users.noreply.github.com>
2019-03-19 15:54:03 +0200

    Project1

---
+++ Project1/scripts/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+    $("#specialists").on("click","a", function (event) {
+
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+
+        var id  = $(this).attr('href'),
+
+ 
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+         
+
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 1500);
+
+    });
+});
+
+$(document).ready(function(){
+
+    $("#navbar").on("click","a", function (event) {
+
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+
+        var id  = $(this).attr('href'),
+
+ 
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+         
+
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 1500);
+
+    });
+});
+
+$(document).ready(function(){
+
+    $("#carousel").on("click","a", function (event) {
+
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+
+        var id  = $(this).attr('href'),
+
+ 
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+         
+
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 1500);
+
+    });
+});
+
+
